d5cb943397a926da434f82369ace63efd1076b84
Nikita <KROLLIKauto94@yandex.ru>
2021-11-01 15:18:38 +0300

    new

---
+++ team_lead-task.js
@@ -0,0 +1,31 @@
+let but = document.querySelector(".button");
+
+but.addEventListener('click', f);
+
+function f(){
+  document.documentElement.scrollTop = 4000;
+}
+
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var fiveMinutes = 60 * 30,
+      display = document.querySelector('#time');
+  startTimer(fiveMinutes, display);
+};
